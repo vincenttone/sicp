af879b4617f034eacf534782021f1bafab156285
vincent <tongwensong@gmail.com>
2016-01-16 08:47:29 +0000

    exercises 3.4

---
+++ chapter-3/exercises/3.4.scm
@@ -0,0 +1,50 @@
+(load "lib.scm")
+
+(define (make-account password balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+	(begin (set! balance (- balance amount))
+	       balance) ; end begin
+	"Insuffcient funds") ; end if
+    ) ; end define
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance) ; end define
+  (define count 0)
+  (define (incr)
+    (set! count (+ count 1))
+    count) ; end define
+  (define (call-the-cops)
+    "Call the cops") ; end define
+  (define (dispatch enter-password m)
+    (if (eq? enter-password password)
+	(cond ((eq? m 'withdraw) withdraw)
+	      ((eq? m 'deposit) deposit)
+	      (else (error "Unknown request -- MAKE-ACCOUNT" m))
+	      ) ; end cond
+	(lambda (x)
+	  (begin (incr)
+		 (if (> count 7) (call-the-cops)
+		     "Incorrect password")
+		 ) ; end begin
+	  ) ; end lambda
+	) ; end if
+    ) ; end define
+  dispatch) ; end define
+
+(define acc (make-account 'secret-password 100))
+
+(p ((acc 'secret-password 'withdraw) 50))
+
+(p ((acc 'some-other-password 'withdraw) 60))
+(p ((acc 'some-other-password 'withdraw) 60))
+(p ((acc 'some-other-password 'withdraw) 60))
+(p ((acc 'some-other-password 'withdraw) 60))
+(p ((acc 'some-other-password 'withdraw) 60))
+(p ((acc 'some-other-password 'withdraw) 60))
+(p ((acc 'some-other-password 'withdraw) 60))
+(p ((acc 'some-other-password 'withdraw) 60))
+
+(p ((acc 'secret-password 'deposit) 40))
+(p ((acc 'secret-password 'withdraw) 60))
+(p ((acc 'secret-password 'withdraw) 80))
