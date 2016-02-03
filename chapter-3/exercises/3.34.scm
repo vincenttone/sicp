79a3cc0a854ca91da183c85687ea7f836207b1e6
vincent <tongwensong@gmail.com>
2016-02-03 14:56:30 +0800

    3.34

---
+++ chapter-3/exercises/3.34.scm
@@ -0,0 +1,41 @@
+;; (multiplier m1 m2 product) 是通过两个已知的值计算第三个值的过程
+;; 开平方时只知道一个product，m1 m2都是未知
+;; 虽然想等却并没这样的约束
+;; 需要额外定义一个约束解决
+
+(define (multiplier m1 m2 product)
+  (define (process-new-value)
+    (cond ((or (and (has-value? m1) (= (get-value m1) 0))
+               (and (has-value? m2) (= (get-value m2) 0)))
+           (set-value! product 0 me))
+          ((and (has-value? m1) (has-value? m2))
+           (set-value! product
+                       (* (get-value m1) (get-value m2))
+                       me))
+          ((and (has-value? product) (has-value? m1))
+           (set-value! m2
+                       (/ (get-value product) (get-value m1))
+                       me))
+          ((and (has-value? product) (has-value? m2))
+           (set-value! m1
+                       (/ (get-value product) (get-value m2))
+                       me))))
+  (define (process-forget-value)
+    (forget-value! product me)
+    (forget-value! m1 me)
+    (forget-value! m2 me)
+    (process-new-value))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)
+           (process-new-value))
+          ((eq? request 'I-lost-my-value)
+           (process-forget-value))
+          (else
+           (error "Unknown request -- MULTIPLIER" request))))
+  (connect m1 me)
+  (connect m2 me)
+  (connect product me)
+  me)
+
+(define (squarer a b)
+  (multiplier a a b))
