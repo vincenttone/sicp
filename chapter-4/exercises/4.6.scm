0ba96e8d57dd42a2e475e54ac5ea4611dce76764
vincent <tongwensong@gmail.com>
2016-03-02 17:42:54 +0800

    4.6

---
+++ chapter-4/exercises/4.6.scm
@@ -0,0 +1,33 @@
+;; (let ((v1 e1) (v2 e2))
+;;   ())
+
+;; ((lambda (v1 v2) ()) e1 e2)
+
+(define (let? exp)
+  (tagged-list? exp 'let))
+
+(define (let-definition exp)
+  (cadr exp)
+  )
+
+(define (let-body exp)
+  (cddr exp)
+  )
+
+(define (let-def-vars definition)
+  (if (null? definition)
+	  '()
+	  (cons (car let-def-var) (let-def-var (cdr definition))))
+  )
+
+(define (let-def-exprs definition)
+  (if (null? definition)
+	  '()
+	  (cons (cdr let-def-var) (let-def-var (cdr definition))))
+  )
+
+(define (let-combination exp env)
+  (list (make-lambda (let-def-vars (let-definition exp))
+					 (let-body exp))
+		(let-def-exprs (let-definition exp)))
+  )
