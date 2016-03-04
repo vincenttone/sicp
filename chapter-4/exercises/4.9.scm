87a8acda8e97cbde4dc6d648d31b94e69dc22053
vincent <tongwensong@gmail.com>
2016-03-04 15:13:26 +0800

    4.9

---
+++ chapter-4/exercises/4.9.scm
@@ -0,0 +1,17 @@
+;; eval 
+((while? expr) (evaln (while->combination  expr) env)) 
+
+;; while
+;; (while (< a 10) (...))
+(define (while? expr) (tagged-list? expr 'while)) 
+(define (while-condition expr) (cadr expr)) 
+(define (while-body expr) (caddr expr)) 
+(define (while->combination expr) 
+  (sequence->exp 
+   (list (list 'define  
+			   (list 'while-iter)
+			   (make-if (while-condition expr)
+						(sequence->exp (list (while-body expr)
+											 (list 'while-iter)))
+						'true))
+		 (list 'while-iter))))
