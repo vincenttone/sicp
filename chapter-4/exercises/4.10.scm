95676da628ddc59e60bb5875d8f01a610337d341
vincent <tongwensong@gmail.com>
2016-03-04 15:51:59 +0800

    4.10

---
+++ chapter-4/exercises/4.10.scm
@@ -0,0 +1,34 @@
+;; ((a b +) (x y >) if)
+;; ((a b -) (a b +) (x y >) if)
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+	  (eq? (last exp) tag)
+	  false))
+
+(define (last exp)
+  (if (null? (cdr exp))
+	  exp
+	  (last (cdr exp)))
+
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+(define (if? exp) (tagged-list? exp 'if))
+(define (if-predicate exp) 
+  (if (null? (cdddr exp))
+	  (cadr exp)
+	  (caddr exp))
+(define (if-consequent exp)
+  (if (null? (cdddr exp))
+	  (car exp)
+	  (cadr exp))
+(define (if-alternative exp)
+  (if (null? (cdddr exp))
+	  'false
+	  (car exp)))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
