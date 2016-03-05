3360c91e9f7a3745e8ed85ada612e64c90fa3cf1
vincent <tongwensong@gmail.com>
2016-03-05 14:36:59 +0800

    update 4.4

--- chapter-4/exercises/4.4.scm
+++ chapter-4/exercises/4.4.scm
@@ -15,8 +15,8 @@
         ((begin? exp)
          (eval-sequence (begin-actions exp) env))
         ((cond? exp) (eval (cond->if exp) env))
-		((and? exp) (eval-and (cdr exp) env))
-		((or? exp) (eval-or (cdr exp) env))
+		((and? exp) (eval-and exp env))
+		((or? exp) (eval-or exp env))
         ((application? exp)
          (apply (eval (operator exp) env)
                 (list-of-values (operands exp) env)))
@@ -24,22 +24,30 @@
          (error "Unknown expression type -- EVAL" exp))))
 
 (define (and? exp)
-  (tagged-list? exp 'and)
+  (tagged-list? exp 'and))
 
 (define (eval-and exp env)
+  (eval (and-if (cdr exp) env) env)
+  )
+
+(define (and-if exp env)
   (if (null? exp) 'true
 	  (make-if (eval (first-exp exp) env)
-			   (eval-and (rest-exp exp) env)
+			   (and-if (rest-exps exp) env)
 			   'false)))
 
-(define (and? exp)
-  (tagged-list? exp 'and)
+(define (or? exp)
+  (tagged-list? exp 'or))
 
 (define (eval-or exp env)
+  (eval (or-if (cdr exp) env) env)
+  )
+
+(define (or-if exp env)
   (if (null? exp) 'false
 	  (make-if (eval (first-exp exp) env)
 			   'true
-			   (eval-or (rest-exp exp) env))))
+			   (eval-or (rest-exps exp) env))))
 
 
 ;;;;
